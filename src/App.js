257bd7feb1692b512701dd5f363ddac95c746df9
Yana <33156136+YanaSezko@users.noreply.github.com>
2020-07-02 12:20:21 +0300

    try_catch

--- src/App.js
+++ src/App.js
@@ -16,57 +16,58 @@ class App extends React.Component {
     wind: undefined,
     error: undefined
   }
-
-  gettingWeather = async(e) => {
-    try{
-    e.preventDefault();
-    let city = e.target.elements.city.value;
-       
-    const api_url= await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric&lang=ru`)
-    const data = await api_url.json();
-    if(city){
-      this.setState({
-         city:data.name,  
-         temp: data.main.temp,
-         country:data.sys.country,
-         description: data.weather[0].description,
-         icon: data.weather[0].icon,
-         wind: data.wind.speed,
-         error: undefined
-         });
-    }else{
-      this.setState({
-        city:undefined,  
-        temp: undefined,
-        country:undefined,
-        description:undefined,
-        icon: undefined,
-        wind: undefined,
-        error: "Введите город"
-        });
-    }
-  }   
-    catch(error){
- this.setState({
-      city: undefined,
+      
+gettingWeather = async(e) => {
+  e.preventDefault();
+  let city = e.target.elements.city.value;
+  try{
+  const api_url= await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric&lang=ru`)
+  const data = await api_url.json();
+  
+  if(city){
+    this.setState({
+       city:data.name,  
+       temp: data.main.temp,
+       country:data.sys.country,
+       description: data.weather[0].description,
+       icon: data.weather[0].icon,
+       wind: data.wind.speed,
+       error: undefined
+       });
+  }else {
+    this.setState({
+      city:undefined,  
       temp: undefined,
-      country: undefined,
+      country:undefined,
       description:undefined,
-      icon:undefined,
+      icon: undefined,
       wind: undefined,
-      error: "Ошибка(Возможно такого города нет в базе данных)"});
+      error: "Введите город"
+      });
   }
-  }
+}   
+  catch(error){
+this.setState({
+    city: undefined,
+    temp: undefined,
+    country: undefined,
+    description:undefined,
+    icon:undefined,
+    wind: undefined,
+    error: "Города нет в базе данных"});
+}
 
-  render(){
+}
+
+render(){
     const iconUrl = "http://openweathermap.org/img/w/" + this.state.icon + ".png";
     return(
       <div className="wrapper">
         <div className="main">
         <div className="container">
           <div className="row">
-            <div className = "col-sm-5 info"><Info/></div>
-            <div className="col-sm-7 form">
+            <div className = "col-xs-6 col-sm-5 info"><Info/></div>
+            <div className="col-xs-6 col-sm-7 form">
               <Form  weatherMethod={this.gettingWeather}/>
               <Weather
               temp={this.state.temp}
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -4,13 +4,14 @@ const Weather = props => (
     <div className="infoWeath">
     {props.city &&    
     <>
-        <p>Местоположение: {props.city}, {props.country}</p>
-        <p>Температура: {props.temp}°С</p>
-        <p>Описание: {props.description}
+     <p>Описание: {props.description}
         <img  src={props.icon}></img>
         </p>
+        <p>Местоположение: {props.city}, {props.country}</p>
+        <p>Температура: {props.temp}°С</p>
         <p>Ветер: {props.wind}м/с</p>
-    </>
+       
+       </>
     }  
     <p className="error">{props.error }</p>    
     </div>
